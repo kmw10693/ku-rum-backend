cba7416203902fdcc13ed62e65d345c985928325
kmw10693 <kmw106933@naver.com>
2025-01-31 00:08:30 +0900

    refactor: 유저 검증 과정 리팩토링 (#55)

--- backend/src/main/java/ku_rum/backend/domain/user/domain/repository/UserRepository.java
+++ backend/src/main/java/ku_rum/backend/domain/user/domain/repository/UserRepository.java
@@ -13,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
     Optional<User> findUserByNickname(String nickname);
     Optional<User> findUserByEmail(String email);
+
 }
