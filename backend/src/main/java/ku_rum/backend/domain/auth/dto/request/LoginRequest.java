2468c37f31b5426a4cf43fdc9f8a7c483a2f11c3
Minu Kim <kmw106933@naver.com>
2025-02-08 19:49:25 +0900

    refactor: Auth API 리팩토링 (#59)

--- backend/src/main/java/ku_rum/backend/domain/auth/dto/request/LoginRequest.java
+++ backend/src/main/java/ku_rum/backend/domain/auth/dto/request/LoginRequest.java
@@ -3,7 +3,7 @@ package ku_rum.backend.domain.auth.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-        @NotBlank(message = "사용자 이메일은 필수입니다.") String email,
+        @NotBlank(message = "사용자 아이디는 필수입니다.") String loginId,
         @NotBlank(message = "사용자 비밀번호는 필수입니다.") String password
 ) {
 }
