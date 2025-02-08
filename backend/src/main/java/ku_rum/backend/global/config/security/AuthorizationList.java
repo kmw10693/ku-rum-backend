a33f233bb1032416a4140121378c16b2ef615490
Minu Kim <kmw106933@naver.com>
2025-02-08 19:49:23 +0900

    refactor: Auth API 리팩토링 (#59)

--- backend/src/main/java/ku_rum/backend/global/config/security/AuthorizationList.java
+++ backend/src/main/java/ku_rum/backend/global/config/security/AuthorizationList.java
@@ -1,9 +1,12 @@
 package ku_rum.backend.global.config.security;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+@Getter
 public enum AuthorizationList {
     LIST(Collections.unmodifiableList(Arrays.asList(
             "/api/v1/auth/login",
@@ -11,21 +14,19 @@ public enum AuthorizationList {
             "/docs/**",
             "/api/v1/users/validations/loginId",
             "/api/v1/users/weinlogin",
-            "/api/v1/users/reset-account"
+            "/api/v1/users/reset-account",
+            "/api/v1/users/join",
+            "/api/v1/mails/auth-codes",
+            "/api/v1/mails/verification_codes"
     )));
 
-    private List<String> authorities;
+    private final List<String> authorities;
 
     AuthorizationList(List<String> authorities) {
         this.authorities = authorities;
     }
 
-    public List<String> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public String toString() {
-        return String.join(", ", authorities);
+    public String[] getAuthorities() {
+        return authorities.toArray(new String[0]);
     }
 }
