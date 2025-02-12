4c709cd2d8b2221459ec98e3beedb34832dc41bd
Hyeri1ee <haerizian10@gmail.com>
2025-02-12 21:24:38 +0900

    feat: BuildingDocument 관련 json추가

---
+++ backend/src/main/java/ku_rum/backend/document/BuildingDocument.java
@@ -0,0 +1,39 @@
+package ku_rum.backend.document;
+
+import jakarta.persistence.Id;
+import ku_rum.backend.domain.building.domain.Building;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Setting;
+import org.springframework.data.elasticsearch.annotations.WriteTypeHint;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(indexName = "posts", writeTypeHint = WriteTypeHint.FALSE)
+@Setting(settingPath = "elastic/building-setting.json")
+@Mapping(mappingPath = "elastic/building-mapping.json")
+public class BuildingDocument {
+  @Id
+  private Long id;
+  private String name;
+  private String abbreviation;
+
+  @Builder
+  public BuildingDocument(Long id, String name, String abbreviation) {
+    this.id = id;
+    this.name = name;
+    this.abbreviation = abbreviation;
+  }
+
+  public static BuildingDocument from(Building building){
+    return BuildingDocument.builder()
+            .id(building.getId())
+            .name(building.getName())
+            .abbreviation(building.getAbbreviation())
+            .build();
+  }
+}
