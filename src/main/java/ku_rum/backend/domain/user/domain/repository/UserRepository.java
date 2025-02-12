24e18cf4f666f0682eaac67ce425d5a68a3c04c6
Minu Kim <kmw106933@naver.com>
2025-02-12 19:34:12 +0900

    refactor: 유저 API 수정 (#98)

--- src/main/java/ku_rum/backend/domain/user/domain/repository/UserRepository.java
+++ src/main/java/ku_rum/backend/domain/user/domain/repository/UserRepository.java
@@ -9,8 +9,10 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByStudentId(String studentId);
+    boolean existsByLoginId(String loginId);
     boolean existsByEmail(String email);
     Optional<User> findUserById(Long id);
     Optional<User> findUserByNickname(String nickname);
     Optional<User> findUserByEmail(String email);
+    Optional<User> findUserByLoginId(String loginId);
 }
