a62dace30278b931256829921d81bfe74851ddae
Hyeri1ee <haerizian10@gmail.com>
2025-03-16 01:49:33 +0900

    chore: 빠진 파일 추가(#144)

---
+++ src/main/java/ku_rum/backend/domain/building/dto/response/BuildingResponse.java
@@ -0,0 +1,34 @@
+package ku_rum.backend.domain.building.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import ku_rum.backend.domain.building.domain.Building;
+
+import java.math.BigDecimal;
+
+public record BuildingResponse(
+        Long buildingId,
+        String buildingName,
+        Long buildingNumber,
+        String buildingAbbreviation,
+        BigDecimal latitude,
+        BigDecimal longitude
+) {
+
+    @QueryProjection
+    public BuildingResponse {
+
+    }
+
+
+
+    public static BuildingResponse of(Building building) {
+        return new BuildingResponse(
+                building.getId(),
+                building.getName(),
+                building.getNumber(),
+                building.getAbbreviation(),
+                building.getLatitude(),
+                building.getLongitude()
+        );
+    }
+}
