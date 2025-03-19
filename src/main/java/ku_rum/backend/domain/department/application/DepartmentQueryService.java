40ea69e3c9b3fbddfd00a34909ed585c433b16d5
Minu Kim <kmw106933@naver.com>
2025-03-19 14:49:28 +0900

    refactor: 도메인 중심을 위한 CQRS 설계 (#181)

---
+++ src/main/java/ku_rum/backend/domain/department/application/DepartmentQueryService.java
@@ -0,0 +1,27 @@
+package ku_rum.backend.domain.department.application;
+
+import ku_rum.backend.domain.department.domain.Department;
+import ku_rum.backend.domain.department.domain.repository.DepartmentRepository;
+import ku_rum.backend.domain.user.dto.request.UserSaveRequest;
+import ku_rum.backend.global.exception.department.NoSuchDepartmentException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static ku_rum.backend.global.support.status.BaseExceptionResponseStatus.NO_SUCH_DEPARTMENT;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
+public class DepartmentQueryService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public Department getDepartment(final UserSaveRequest userSaveRequest) {
+        log.debug("학과 정보 검색: department={}", userSaveRequest.department());
+        return departmentRepository.findFirstByName(userSaveRequest.department())
+                .orElseThrow(() -> new NoSuchDepartmentException(NO_SUCH_DEPARTMENT));
+    }
+}
