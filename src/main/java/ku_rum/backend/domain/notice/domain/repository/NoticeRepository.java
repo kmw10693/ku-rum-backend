97de78211609febee964e5b8ada7e9e4cb25a332
janghyunjun <buzz03312@naver.com>
2025-03-25 19:04:42 +0900

    refactor: 페이징 처리 쿼리 추가 및 querydsl 적용 (#135)

--- src/main/java/ku_rum/backend/domain/notice/domain/repository/NoticeRepository.java
+++ src/main/java/ku_rum/backend/domain/notice/domain/repository/NoticeRepository.java
@@ -2,6 +2,8 @@ package ku_rum.backend.domain.notice.domain.repository;
 
 import ku_rum.backend.domain.notice.domain.Notice;
 import ku_rum.backend.domain.notice.domain.NoticeCategory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,13 +13,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface NoticeRepository extends JpaRepository<Notice, String> {
-    List<Notice> findByNoticeCategory(NoticeCategory noticeCategory);
+public interface NoticeRepository extends JpaRepository<Notice, String>, NoticeRepositoryCustom {
+//    List<Notice> findByNoticeCategory(NoticeCategory noticeCategory);
+
+    //페이징 처리
+    List<Notice> findByNoticeCategoryOrderByDateDesc(NoticeCategory noticeCategory, Pageable pageable);
 
     Optional<Notice> findByUrl(String link);
-
-    @Query("SELECT n FROM Notice n WHERE LOWER(n.title) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
-    List<Notice> searchNoticesByTitle(@Param("searchTerm") String searchTerm);
 
     boolean existsByUrl(String url);
 }
---
+++ src/main/java/ku_rum/backend/domain/notice/domain/repository/NoticeRepositoryImpl.java
@@ -0,0 +1,41 @@
+package ku_rum.backend.domain.notice.domain.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import ku_rum.backend.domain.notice.domain.Notice;
+import ku_rum.backend.domain.notice.domain.QNotice;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class NoticeRepositoryImpl implements NoticeRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    private final QNotice qNotice = QNotice.notice;
+
+//    @Query("SELECT n FROM Notice n WHERE LOWER(n.title) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+//    List<Notice> searchNoticesByTitle(@Param("searchTerm") String searchTerm);
+    //를 Querydsl로 리팩토링해보자!
+    public List<Notice> searchNoticesByTitle(String searchTerm) {   //해당 검색어로 모든 공지사항 결과 가져오는 쿼리
+        return queryFactory
+                .selectFrom(qNotice)
+                .where(qNotice.title.lower().likeIgnoreCase("%" + searchTerm + "%"))
+                .fetch();
+    }
+
+    // 페이징 처리
+    @Override
+    public List<Notice> searchNoticesByTitleWithPaging(String searchTerm, int page, int pageSize) {
+        return queryFactory
+                .selectFrom(qNotice)
+                .where(qNotice.title.lower().likeIgnoreCase("%" + searchTerm + "%"))
+                .orderBy(qNotice.date.desc())
+                .offset((long) page * pageSize)
+                .limit(pageSize)
+                .fetch();
+    }
+}
