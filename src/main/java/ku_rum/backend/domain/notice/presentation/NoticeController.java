5ba7196af8532fecc32be815b1d3d8b608fb449b
janghyunjun <buzz03312@naver.com>
2025-03-25 19:06:25 +0900

    refactor: 공지사항 API 페이징 처리 (#135)

--- src/main/java/ku_rum/backend/domain/notice/presentation/NoticeController.java
+++ src/main/java/ku_rum/backend/domain/notice/presentation/NoticeController.java
@@ -19,22 +19,24 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     /**
-     * 카테고리별 공지사항 조회
-     * @param category
+     * 카테고리별 공지사항 조회 (페이징 처리)
+     * @param category, page(page는 1부터)
      * @return
      */
     @GetMapping
-    public BaseResponse<List<NoticeSimpleResponse>> getNoticesByCategory(@RequestParam(name = "category") NoticeCategory category) {
-        return BaseResponse.ok(noticeService.findNoticesByCategory(category));
+    public BaseResponse<List<NoticeSimpleResponse>> getNoticesByCategory(@RequestParam(name = "category") NoticeCategory category,
+                                                                         @RequestParam(name = "page") int page) {
+        return BaseResponse.ok(noticeService.findNoticesByCategory(category, page));
     }
 
     /**
-     * 공지사항 제목을 통한 검색
-     * @param searchTerm
+     * 공지사항 제목을 통한 검색 (페이징 처리)
+     * @param searchTerm, page(page는 1부터)
      * @return
      */
     @GetMapping("/search")
-    public BaseResponse<List<NoticeSimpleResponse>> searchNotices(@RequestParam(name = "searchTerm") String searchTerm) {
-        return BaseResponse.ok(noticeService.searchNoticesByTitle(searchTerm));
+    public BaseResponse<List<NoticeSimpleResponse>> searchNotices(@RequestParam(name = "searchTerm") String searchTerm,
+                                                                  @RequestParam(name = "page") int page) {
+        return BaseResponse.ok(noticeService.searchNoticesByTitle(searchTerm, page));
     }
 }
