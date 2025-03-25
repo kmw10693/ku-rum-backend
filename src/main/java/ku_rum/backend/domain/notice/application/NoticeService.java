01f427ea3265065679da66fbaf322e89847b5bf6
janghyunjun <buzz03312@naver.com>
2025-03-25 19:06:16 +0900

    refactor: 공지사항 페이징 처리 (#135)

--- src/main/java/ku_rum/backend/domain/notice/application/NoticeService.java
+++ src/main/java/ku_rum/backend/domain/notice/application/NoticeService.java
@@ -2,16 +2,20 @@ package ku_rum.backend.domain.notice.application;
 
 import ku_rum.backend.domain.notice.domain.Notice;
 import ku_rum.backend.domain.notice.domain.NoticeCategory;
-import ku_rum.backend.domain.notice.domain.NoticeStatus;
 import ku_rum.backend.domain.notice.domain.repository.NoticeRepository;
 import ku_rum.backend.domain.notice.dto.response.NoticeSimpleResponse;
+import ku_rum.backend.global.exception.notice.InvalidPageException;
+import ku_rum.backend.global.support.status.BaseExceptionResponseStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static ku_rum.backend.global.support.status.BaseExceptionResponseStatus.*;
 
 @Service
 @Slf4j
@@ -20,14 +24,20 @@ public class NoticeService {
 
     private final NoticeRepository noticeRepository;
 
+    private final int PAGE_SIZE = 20;       //한 페이지에 들어갈 공지사항 개수 (추후에 논의)
+
     @Qualifier("urlRedisTemplate")
     private final RedisTemplate<String, String> urlRedisTemplate;
 
     /**
      * 카테고리별 공지사항 조회
      */
-    public List<NoticeSimpleResponse> findNoticesByCategory(NoticeCategory category) {
-        List<Notice> notices = noticeRepository.findByNoticeCategory(category);
+    //todo paging 예외처리
+    public List<NoticeSimpleResponse> findNoticesByCategory(NoticeCategory category, int page) {
+
+        validatePage(page);
+
+        List<Notice> notices = noticeRepository.findByNoticeCategoryOrderByDateDesc(category, PageRequest.of(page - 1, PAGE_SIZE));
         return notices.stream()
                 .map(NoticeSimpleResponse::new)
                 .toList();
@@ -36,12 +46,22 @@ public class NoticeService {
     /**
      * 제목으로 공지사항 검색
      */
-    public List<NoticeSimpleResponse> searchNoticesByTitle(String searchTerm) {
-        List<Notice> notices = noticeRepository.searchNoticesByTitle(searchTerm.trim());
+    //todo paging 예외처리
+    public List<NoticeSimpleResponse> searchNoticesByTitle(String searchTerm, int page) {
+
+        validatePage(page);
+
+        List<Notice> notices = noticeRepository.searchNoticesByTitleWithPaging(searchTerm.trim(), page - 1, PAGE_SIZE);
         return notices.stream()
                 .map(NoticeSimpleResponse::new)
                 .toList();
     }
 
+    private static void validatePage(int page) {
+        //요청으로 들어온 page는 1부터 시작해야 함
+        if (page < 1) {
+            throw new InvalidPageException(INVALID_PAGE);
+        }
+    }
 
 }
