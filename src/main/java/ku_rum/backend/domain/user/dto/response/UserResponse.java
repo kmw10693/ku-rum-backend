fbbc01b74771158b56b99654153a7eed1e09ce7a
Minu Kim <kmw106933@naver.com>
2025-06-01 16:21:45 +0900

    feat: 소셜 로그인 시 최초 로그인 여부 반환

--- src/main/java/ku_rum/backend/domain/user/dto/response/UserResponse.java
+++ src/main/java/ku_rum/backend/domain/user/dto/response/UserResponse.java
@@ -5,9 +5,9 @@ import ku_rum.backend.domain.department.dto.DepartmentResponse;
 import java.util.List;
 
 public record UserResponse(Long id, String oauthId, String loginId, String email, String nickname, String studentId,
-                           String imageUrl, List<DepartmentResponse> departmentResponse) {
+                           String imageUrl, List<DepartmentResponse> departmentResponse, boolean isFirstLogin) {
 
-    public static UserResponse of(Long id, String oauthId, String loginId, String email, String nickname, String studentId, String imageUrl, List<DepartmentResponse> departmentResponse) {
-        return new UserResponse(id, oauthId, loginId, email, nickname, studentId, imageUrl, departmentResponse);
+    public static UserResponse of(Long id, String oauthId, String loginId, String email, String nickname, String studentId, String imageUrl, List<DepartmentResponse> departmentResponse, boolean isFirstLogin) {
+        return new UserResponse(id, oauthId, loginId, email, nickname, studentId, imageUrl, departmentResponse, isFirstLogin);
     }
 }
