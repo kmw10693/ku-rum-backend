3c8dc90fb9857052cf2eb9a58ba6bb3e5335f555
david-parkk <ji0513ji@naver.com>
2025-06-27 16:05:52 +0900

    del: 불필요한 외래키 삭제
    - building_id

--- src/main/java/ku_rum/backend/domain/department/domain/Department.java
+++ src/main/java/ku_rum/backend/domain/department/domain/Department.java
@@ -1,7 +1,6 @@
 package ku_rum.backend.domain.department.domain;
 
 import jakarta.persistence.*;
-import ku_rum.backend.domain.building.domain.Building;
 import ku_rum.backend.domain.college.domain.College;
 import ku_rum.backend.global.support.type.BaseEntity;
 import lombok.*;
@@ -19,25 +18,20 @@ public class Department extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "buliding_id", nullable = false)
-    private Building building;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "college_id")
     private College college;
 
     @Builder
-    private Department(String name, Building building, College college) {
+    private Department(String name, College college) {
         this.name = name;
-        this.building = building;
         this.college = college;
     }
 
-    public static Department of(String name, Building building, College college) {
+    public static Department of(String name, College college) {
         return Department.builder()
                 .name(name)
-                .building(building)
                 .college(college)
                 .build();
     }
