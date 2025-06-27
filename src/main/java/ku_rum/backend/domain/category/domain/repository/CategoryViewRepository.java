f2ea4e2fbb730200cd8fa860f37f481420d2d8ee
david-parkk <ji0513ji@naver.com>
2025-06-27 15:51:14 +0900

    del: 지도 관련 코드 삭제
    - building, category, menu, place 패키지 삭제
    -

--- src/main/java/ku_rum/backend/domain/category/domain/repository/CategoryViewRepository.java
+++
@@ -1,9 +0,0 @@
-package ku_rum.backend.domain.category.domain.repository;
-
-import ku_rum.backend.domain.building.domain.Building;
-import ku_rum.backend.domain.category.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryViewRepository extends JpaRepository<Category, Long> {
-
-}
