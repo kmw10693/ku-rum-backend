34d636191c9f83cd4c2d8d03fc8a2daf807fae9a
david-parkk <ji0513ji@naver.com>
2025-07-10 00:33:12 +0900

    feat: 엔드포인트 허가

--- src/main/java/ku_rum/backend/global/config/AuthorizationList.java
+++ src/main/java/ku_rum/backend/global/config/AuthorizationList.java
@@ -31,8 +31,7 @@ public final class AuthorizationList {
             "/api/v1/buildings/**",
             "/api/v1/departments/**",
             "/api/v1/colleges/**",
-            "/api/v1/places/search",
-            "/chip/**"
+            "/api/v1/places/chip"
     );
 
     private AuthorizationList() {
