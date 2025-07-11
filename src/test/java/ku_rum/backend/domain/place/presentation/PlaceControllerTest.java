45344f04b6cfca07e8cdf14b88ef4403288a9c17
david-parkk <ji0513ji@naver.com>
2025-07-11 16:03:19 +0900

    fix: 테스트 변경
    - 엔티티 생성자 추가

--- src/test/java/ku_rum/backend/domain/place/presentation/PlaceControllerTest.java
+++ src/test/java/ku_rum/backend/domain/place/presentation/PlaceControllerTest.java
@@ -1,9 +1,6 @@
 package ku_rum.backend.domain.place.presentation;
 
-<<<<<<< HEAD
 import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
-=======
->>>>>>> fc5536cdf3146cfcbc7cde2deee0055e03f890ca
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -20,26 +17,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
-<<<<<<< HEAD
 import java.util.List;
+import java.util.Map;
 import ku_rum.backend.config.RestDocsTestSupport;
 import ku_rum.backend.domain.place.application.PlaceService;
 import ku_rum.backend.domain.place.application.PositionService;
 import ku_rum.backend.domain.place.domain.CategoryChip;
 import ku_rum.backend.domain.place.domain.Place;
-=======
-import ku_rum.backend.config.RestDocsTestSupport;
-import ku_rum.backend.domain.place.application.PositionService;
->>>>>>> fc5536cdf3146cfcbc7cde2deee0055e03f890ca
+import ku_rum.backend.domain.place.domain.PlaceImage;
 import ku_rum.backend.domain.place.dto.request.CurrentPositionConfirmRequest;
 import ku_rum.backend.domain.place.dto.request.CurrentPositionRequest;
 import ku_rum.backend.domain.place.dto.response.CurrentPositionConfirmResponse;
 import ku_rum.backend.domain.place.dto.response.CurrentPositionResponse;
 import ku_rum.backend.domain.place.dto.response.CurrentPositionStatusResponse;
-<<<<<<< HEAD
 import ku_rum.backend.domain.place.dto.response.SelectPlaceChipResponse;
-=======
->>>>>>> fc5536cdf3146cfcbc7cde2deee0055e03f890ca
 import ku_rum.backend.domain.user.domain.User;
 import ku_rum.backend.global.security.CustomUserDetails;
 import org.junit.jupiter.api.BeforeEach;
@@ -61,12 +52,9 @@ public class PlaceControllerTest extends RestDocsTestSupport {
     PositionService positionService;
 
     @MockBean
-<<<<<<< HEAD
     PlaceService placeService;
 
     @MockBean
-=======
->>>>>>> fc5536cdf3146cfcbc7cde2deee0055e03f890ca
     private SecurityFilterChain securityFilterChain;
 
     @BeforeEach
@@ -197,7 +185,6 @@ public class PlaceControllerTest extends RestDocsTestSupport {
                                 )
                                 .build())));
     }
-<<<<<<< HEAD
 
     @DisplayName("칩에 해당 하는 정보를 조회한다")
     @Test
@@ -214,7 +201,12 @@ public class PlaceControllerTest extends RestDocsTestSupport {
                 .longitude(BigDecimal.valueOf(127.073784000))
                 .build();
 
-        List<SelectPlaceChipResponse> response = List.of(SelectPlaceChipResponse.from(place));
+        List<PlaceImage> placeImages = List.of(PlaceImage.builder()
+                .imageUrl("imgurl.png")
+                .build());
+        Map<Long, List<PlaceImage>> map = Map.of(1L, placeImages);
+
+        List<SelectPlaceChipResponse> response = List.of(SelectPlaceChipResponse.from(place, map));
 
         given(placeService.selectChipWithUser(any(CustomUserDetails.class), eq(CategoryChip.K_CUBE)))
                 .willReturn(response);
@@ -238,6 +230,4 @@ public class PlaceControllerTest extends RestDocsTestSupport {
                                 .queryParameters(parameterWithName("chip").description("칩 이름"))
                                 .build())));
     }
-=======
->>>>>>> fc5536cdf3146cfcbc7cde2deee0055e03f890ca
 }
