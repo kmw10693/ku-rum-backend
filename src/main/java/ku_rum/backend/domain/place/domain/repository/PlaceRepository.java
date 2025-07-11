dbcbfe4c898150a4dae083b55dc79f9b6780690b
david-parkk <ji0513ji@naver.com>
2025-07-11 14:44:44 +0900

    feat: @Repository 어노테이션 추가

--- src/main/java/ku_rum/backend/domain/place/domain/repository/PlaceRepository.java
+++ src/main/java/ku_rum/backend/domain/place/domain/repository/PlaceRepository.java
@@ -8,7 +8,9 @@ import ku_rum.backend.domain.place.domain.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
     @Query("""
--- src/main/java/ku_rum/backend/domain/place/domain/repository/PositionRepository.java
+++ src/main/java/ku_rum/backend/domain/place/domain/repository/PositionRepository.java
@@ -8,7 +8,9 @@ import ku_rum.backend.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
 
     boolean existsPositionByUser(User user);
