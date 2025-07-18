6eb8680f104de4bde808bd64b9d21a9a6c40acaf
Park Jiwon <57484954+david-parkk@users.noreply.github.com>
2025-07-18 11:10:38 +0900

    [FEAT] 지도 검색 히스토리 API (#273)
    * fix: 버그 수정
    
    - 쿼리 수정
    
    * fix: 버그 수정
    
    - 쿼리 추가
    
    * test: 테스트 수정
    
    * feat: 장소 검색 API
    
    * feat: 장소 검색 기록 API
    
    * test: 테스트 코드 추가

---
+++ src/main/java/ku_rum/backend/domain/place/domain/PlaceHistory.java
@@ -0,0 +1,41 @@
+package ku_rum.backend.domain.place.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import ku_rum.backend.domain.user.domain.User;
+import ku_rum.backend.global.support.type.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
+public class PlaceHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long placeHistoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Place place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private User user;
+
+    public void refreshModifiedAt() {
+        this.modifiedAt = LocalDateTime.now();
+    }
+}
