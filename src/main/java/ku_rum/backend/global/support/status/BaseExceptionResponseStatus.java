34aa9fa89e1796f9b039c67ad72dd810ee8d3f18
Minu Kim <kmw106933@naver.com>
2025-12-05 23:29:18 +0900

    [FEAT] 소셜 로그인 - 애플 로그인 추가 (#344)
    * [feat] 소셜 로그인 - 애플 로그인 추가
    
    * [feat] 소셜 로그인 - 애플 로그인 추가
    
    * resolve: take develop submodule pointer for config
    
    * [refactor] 포멧팅 수정
    
    * [refactor] 포맷팅 enum 예외로 변경
    
    * [refactor] 구글 URI 제거
    
    * [refactor] AppleUtils에 로깅 추가
    
    * [refactor] 테스트 컨픽 제거

--- src/main/java/ku_rum/backend/global/support/status/BaseExceptionResponseStatus.java
+++ src/main/java/ku_rum/backend/global/support/status/BaseExceptionResponseStatus.java
@@ -104,6 +104,7 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     TOKEN_MISMATCH(1006, HttpStatus.UNAUTHORIZED, "로그인 정보가 토큰 정보와 일치하지 않습니다."),
     LOGIN_ERROR(1007, HttpStatus.UNAUTHORIZED, "잘못된 아이디/비밀번호입니다."),
     OAUTH_ERROR(1008, HttpStatus.UNAUTHORIZED, "잘못된 임시 토큰입니다."),
+    OAUTH_INVALID_PRIVATE_KEY(1009, HttpStatus.UNAUTHORIZED, "잘못된 프라이빗 키입니다."),
 
     /**
      * 1100: Wein
